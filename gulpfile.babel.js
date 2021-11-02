4dbd5aa724f17f81d5c5d6ed20f0d047376fb21e
jinsunkim <jinsunkim94@gmail.com>
2021-11-02 22:42:00 +0900

    Config gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,27 @@
+"use strict";
 import gulp from "gulp";
-import babel from "gulp-babel";
-import ghtml from "gulp-html";
+import del from "del";
+var gsass = require("gulp-sass")(require("sass"));
+import gimage from "gulp-image";
 
-gulp
-  .task("default", () =>
-    //for babel
-    gulp.src("src/app.js").pipe(
-      babel({
-        presets: ["@babel/env"],
-      })
-    )
-  )
-  .pipe(gulp.dest("build/"));
-
-const html = () => {
-  return gulp.src("src/index.html").pipe(ghtml()).pipe(gulp.dest("build/"));
+const routes = {
+  html: {
+    src: "src/*.html",
+    dest: "build",
+  },
+  scss: {
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
 };
+//Build
+export const buildHtml = () =>
+  gulp.src(routes.html.src).pipe(gulp.dest(routes.html.dest));
+export const buildSass = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(gsass().on("error", gsass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+//Cleaning Folder
+export const clean = () => del(["build"]);
+export const dev = gulp.series([clean, buildHtml, buildSass]);
