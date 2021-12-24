a40bdfc86f76b265bc827c84d4287d7375a229f9
jinsunkim <jinsunkim94@gmail.com>
2021-12-25 02:12:43 +0900

    Fixing convert image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,9 @@ import gulp from "gulp";
 import htmlmin from "gulp-htmlmin";
 const sass = require("gulp-sass")(require("sass"));
 import csso from "gulp-csso";
-import concat from "gulp-concat";
-import image from "gulp-image";
+import gbro from "gulp-bro";
+import imagemin from "gulp-image";
+import babelify from "babelify";
 import del from "del";
 import webserver from "gulp-webserver";
 
@@ -21,12 +22,12 @@ const routes = {
     watch: "src/scss/**.scss",
   },
   js: {
-    src: "src/js/*.js",
+    src: "src/js/main.js",
     dest: "build/js/",
-    watch: "src/js/**.js",
+    watch: "src/js/**/*.js",
   },
   img: {
-    src: "src/img/**",
+    src: "src/img/*",
     dest: "build/img/",
   },
   reset: {
@@ -50,15 +51,22 @@ const buildStyle = () => {
     .pipe(gulp.dest(routes.sass.dest));
 };
 
-const buildConcat = () => {
+const buildGbro = () => {
   return gulp
     .src(routes.js.src)
-    .pipe(concat("main.js"))
+    .pipe(
+      gbro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
     .pipe(gulp.dest(routes.js.dest));
 };
 
 const buildImg = () => {
-  return gulp.src(routes.img.src).pipe(image()).pipe(routes.img.dest);
+  gulp.src(routes.img.src).pipe(imagemin()).pipe(gulp.dest("routes.img.dest"));
 };
 
 const buildReset = () => {
@@ -72,7 +80,8 @@ const buildReset = () => {
 const watch = () => {
   gulp.watch(routes.sass.watch, buildStyle);
   gulp.watch(routes.html.watch, buildIndex);
-  gulp.watch(routes.js.watch, buildJs);
+  gulp.watch(routes.img.src, buildImg);
+  gulp.watch(routes.js.watch, buildGbro);
 };
 
 const webServer = () =>
@@ -87,7 +96,7 @@ const liveServer = gulp.series([webServer, watch]);
 export const dev = gulp.series([
   buildIndex,
   buildStyle,
-  buildConcat,
+  buildGbro,
   buildImg,
   buildReset,
   liveServer,
