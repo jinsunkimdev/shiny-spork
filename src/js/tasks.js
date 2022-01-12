e441b8f9d4e0884cc76a87ea42ee4f08804e1189
jinsunkim <jinsunkim94@gmail.com>
2022-01-12 23:46:26 +0900

    Add remove TODO list function

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -4,14 +4,16 @@ const tasksForm = document.querySelector("#tasks__input--form");
 const tasksList = document.getElementById("tasks__list");
 const tasksInput = document.querySelector("#tasks__input--form input");
 const tasksBtn = document.getElementById("tasks__btn");
-const taskItem = document.createElement("li");
-const taskCheckBtn = document.createElement("div");
-const taskText = document.createElement("span");
-const taskInputValue = tasksInput.value;
+const mainContainer = document.querySelector(".main__container");
 
 //function
+/*listing TODO*/
 const printTasks = (clickButton) => {
   clickButton.preventDefault();
+
+  const taskItem = document.createElement("li");
+  const taskCheckBtn = document.createElement("div");
+  const taskText = document.createElement("span");
   taskItem.setAttribute("class", "task__item");
   taskCheckBtn.setAttribute("class", "task__item--check");
   taskText.setAttribute("class", "task__item--text");
@@ -19,17 +21,35 @@ const printTasks = (clickButton) => {
   tasksList.appendChild(taskItem);
   taskItem.append(taskCheckBtn, taskText);
 
+  const taskInputValue = tasksInput.value;
   taskText.innerText = taskInputValue;
   tasksInput.value = "";
-
-  removeTask(tasksBtn);
+  if (tasksList.childNodes.length == 1) {
+    setTimeout(() => {
+      mainContainer.classList.add("task_list_empty");
+    }, 500);
+  } else
+    setTimeout(() => {
+      mainContainer.classList.remove("task_list_empty");
+    });
+  removeTask(taskCheckBtn);
 };
+
 tasksForm.addEventListener("submit", printTasks);
 
-const removeTask = (checkedBtn) => {
-  checkedBtn.addEventListener("click", (what) => {
-    what.preventDefault();
-    checkedBtn.remove;
-    checkedBtn.parentElement.remove;
+/*remove TODO*/
+const removeTask = (taskCheckBtn) => {
+  taskCheckBtn.addEventListener("click", (e) => {
+    let parent = e.target.parentElement;
+    parent.classList.add("task-completed");
+    setTimeout(() => {
+      parent.remove();
+    }, 400);
+
+    if (tasksList.childNodes.length == 1) {
+      setTimeout(() => {
+        mainContainer.classList.add("task_list_empty");
+      }, 500);
+    }
   });
 };
